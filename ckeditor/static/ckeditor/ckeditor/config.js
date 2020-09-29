a719a8b36ecdfc8b4bd7b2160fb3dbddc6c9c96b
Julian Pedro F. Braga <julian.pedro@hotmail.com>
2020-09-29 12:47:29 -0400

    Update config.js

--- ckeditor/static/ckeditor/ckeditor/config.js
+++ ckeditor/static/ckeditor/ckeditor/config.js
@@ -7,6 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.extraAllowedContent = 'span(*)';
 };
 
 CKEDITOR.dtd.$removeEmpty['span'] = false;
